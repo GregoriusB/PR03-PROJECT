8ac67a992c0a283a183ef53c1a22b5dfe49f6996
KJ <kj@student-10-201-19-060.wireless.adelaide.edu.au>
2022-10-10 14:54:52 +1030

    Forgot to add

---
+++ SaveSystemDriver.cpp
@@ -0,0 +1,11 @@
+#include "SaveSystem.h"
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+	SaveSystem saving;
+	saving.loadProgress();
+}
