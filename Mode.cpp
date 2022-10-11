fe9a5e3229916cfbf8324a71e564c16765086248
KJ <kj@student-10-201-18-188.wireless.adelaide.edu.au>
2022-10-11 11:08:51 +1030

    Comments

--- Mode.cpp
+++ Mode.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-
 #include "GameCode.h"
-
 using namespace std;
 
 // Determines whether the player is ready to commence the game.
 void Mode::is_player_ready() {
+  // while loop to ensure player correctly inputs
   while (PlayerReady != "Y") {
     cout << "Are your ready to commence the game (Y): ";
     cin >> PlayerReady;
@@ -15,6 +14,7 @@ void Mode::is_player_ready() {
 
 // Number of Players
 string Mode::Number_of_Players() {
+  // While loop to make sure player enters a valid number of players
   while ((NumPlayers != "1") && (NumPlayers != "2")) {
     cout << "How many Players (1 or 2): ";
     cin >> NumPlayers;
