c11d1790180a999ddf258279d9b2ce2e29c1b16a
Luke <luke@MSI>
2022-10-15 09:44:18 +1030

    Changes to singleplayer

--- CPP Files/Singleplayer.cpp
+++ CPP Files/Singleplayer.cpp
@@ -12,13 +12,13 @@ using namespace std;
 
 string Singleplayer::getDifficulty() {
   // ask user what difficulty level they want to play
-  cout << "Enter difficulty level : " << endl;
+  cout << "Enter difficulty level (Easy, Hard): ";
   cin >> difficulty;
   // use while loop to keep asking the user easy or hard difficulty
-  while (difficulty != "Easy" || difficulty != "Hard") {
+  while (difficulty != "Easy" && difficulty != "Hard") {
     // I will fix the the code so that it would take both uppercase and
     // lowercase words
-    cout << "Enter difficulty level : " << endl;
+    cout << "Enter difficulty level (Easy, Hard): ";
     cin >> difficulty;
   }
   return difficulty;
