75aca9fa9a12c74f308dd79023d1fdbab00028bb
Luke <luke@MSI>
2022-10-16 11:25:57 +1030

    Changes to folder directories and finished singleplayer

--- Header Files/Player.h
+++ Header Files/Player.h
@@ -18,4 +18,55 @@ class Multiplayer : public Player {
  public:
   string setWord();
 };
+
+class Singleplayer {
+ protected:
+  string difficulty;
+  int flag3 = 0;
+  string WordCategory;
+  string Word;
+
+ public:
+  // function for the cpp files
+  string getDifficulty();
+};
+
+class SinglePlayerEasy : public Singleplayer, public Player {
+ protected:
+  string AussieEasy[7] = {"bloke",   "mate",    "mozzie", "kangaroo",
+                          "crickey", "sunnies", "koala"};
+  string NatureEasy[7] = {"cave",    "environment", "meadow", "rainforest",
+                          "volcano", "desert",      "valley"};
+  string SpaceEasy[7] = {"martian", "jupiter",       "mercury", "interstellar",
+                         "solar",   "intergalactic", "wormhole"};
+  string SportEasy[7] = {"soccer",     "fitness", "skiing",  "coaching",
+                         "gymnastics", "esports", "baseball"};
+
+ private:
+  int flag = 0;
+  string WordCategory;
+
+ public:
+  // function for the cpp files
+  string setWord();
+};
+
+class SinglePlayerHard : public Singleplayer, public Player {
+ protected:
+  string AussieHard[7] = {"brekkie", "bathers", "heaps",  "battler",
+                          "servo",   "yakka",   "outback"};
+  string NatureHard[7] = {"photosynthesis", "pangaea", "stalacites",
+                          "archipelago",    "glacier", "biosphere",
+                          "continent"};
+  string SpaceHard[7] = {"nucleosynthesis", "andromeda", "geoheliocentrism",
+                         "heliosphere",     "titan",     "centauri",
+                         "extraterrestrial"};
+  string SportHard[7] = {"triathlons",  "lacrosse", "motocross", "paddlesport",
+                         "racquetball", "sledding", "boxing"};
+
+ public:
+  // function for the cpp files
+  string setWord();
+};
+
 #endif /* PLAYER_H */
